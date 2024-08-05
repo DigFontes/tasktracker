e002f62f1d74e1840e33afd8ac23ab1535fae861
Diêgo Fontes <104260227+DigFontes@users.noreply.github.com>
2024-08-05 16:59:51 -0300

    First commit

---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BoxComponenete",
+});
+</script>
+
+<style scoped>
+.box {
+  background: #d3d3d3;
+}
+</style>
